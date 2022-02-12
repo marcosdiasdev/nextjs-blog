55f10e84141856354595c7a34c8c769390957e5f
Marcos Dias da Conceição <ti.marcosdias@gmail.com>
2022-02-12 19:19:33 -0300

    improve tests

--- components/date/index.spec.tsx
+++ components/date/index.spec.tsx
@@ -3,6 +3,14 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 describe("Date", () => {
+  const consoleError = console.error;
+  beforeAll(() => {
+    console.error = jest.fn();
+  });
+  afterAll(() => {
+    console.error = consoleError;
+  });
+
   it("should render formatted date", () => {
     const dateString = "2022-02-11";
     const formattedDate = "February 11, 2022";
@@ -10,7 +18,7 @@ describe("Date", () => {
     expect(screen.getByText(formattedDate)).toBeInTheDocument();
   });
 
-  it("should not render if format is invalid", () => {
+  it("should throw if format is invalid", () => {
     const dateString = "11-02-2022";
     expect(() => render(<DateComponent dateString={dateString} />)).toThrow();
   });
